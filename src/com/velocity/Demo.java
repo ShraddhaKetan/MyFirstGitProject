eb5ac4f0a8afcdc53a71f50554e2a1dbcc4a0840
ShraddhaKetan <shraddha.ingale29@gmail.com>
2023-07-12 16:41:51 +0530

    second commit

--- src/com/velocity/Demo.java
+++ src/com/velocity/Demo.java
@@ -3,5 +3,6 @@ package com.velocity;
 public class Demo {
 public static void main(String[] args) {
 	System.out.println("My first Git Project");
+	System.out.println("Changes done in project"); 
 }
 }
