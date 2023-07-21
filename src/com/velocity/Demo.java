6452dcdbb734f889a75e56798ff4cc390c42c2f5
Sukhada Wagale <sukhadawagale@gmail.com>
2023-07-21 15:55:44 +0530

    changes has been done

--- src/com/velocity/Demo.java
+++ src/com/velocity/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
 public static void main(String[] args) {
 	System.out.println("My first Git Project");
 	System.out.println("Changes done in project"); 
+	System.out.println("My New Git"); 
+	
 }
 }
